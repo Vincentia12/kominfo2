019ae98049600884c900d7ab88d1dc3384e1ef6b
SabrinaHeryantiSuryandari <19081010130@student.upnjatim.ac.id>
2022-12-15 13:23:44 +0700

    perbaikan route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\view;
-use App\Http\Controllers\AsetController;
-use App\Http\Controllers\GuestController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PemilikController;
 
 use App\Http\Controllers\PelaksanaController;
 use App\Http\Controllers\PengadaanController;
@@ -39,44 +36,6 @@ Route::get('/admin', function () {
     return view('index.admin');
 });
 
-// CRUD
-// route untuk input data yg telah terverifikasi
-Route::get('/verified-create', function () {
-    return view('admin.editpemilik');
-});
-
-// route untuk input data yg belum terverifikasi
-Route::get('/show', function () {
-    return view('no-admin.show');
-});
-
-
-// Route::get('/editpemilik', function () {
-//     return view('admin.editpemilik');
-// });
-Route::get('/lihat', function () {
-    return view('admin.lihat');
-});
-
-Route::get('/lihat', function () {
-    return view('admin.lihat');
-});
-
-//baru
-// Route::get('/pengadaan2', function () {
-//     return view('admin.pengadaan2');
-// });
-
-
-// Route::get('/input_anggaran', function () {
-//     return view('admin.input_anggaran');
-// });
-
-// Route::resource('/pengaadaan', PengadaanController::class);
-// Route::get('/input_pengadaan', function () {
-//     return view('admin.inputpengadaan');
-// });
-
 //pelaksana
 Route::resource('/pelaksana', PelaksanaController::class);
 Route::get('/input_pelaksana', [PelaksanaController::class, 'index']);
@@ -96,21 +55,15 @@ Route::post('/postbarang', [BarangController::class, 'store']);
 Route::post('/barang/update/{id}', [BarangController::class, 'update']);
 
 //pejabat
-// Route::get('/pejabat/edit/{id}', [PejabatController::class, 'edit']);
 Route::post('/pejabat/update/{id}', [PejabatController::class, 'update']);
 
 //Pengadaan
 Route::resource('/pengadaan', PengadaanController::class);
-// Route::resource('/pengadaan1', PengadaanController::class);
 Route::get('/input_pengadaan', [PengadaanController::class, 'index']);
 Route::post('/postpengadaan', [PengadaanController::class, 'store']);
 Route::post('/pengadaan/update/{id}', [PengadaanController::class, 'update']);
-// Route::delete('pengadaan/{id}', [PengadaanController::class, 'destroy']);
 
-
-// Route::get('/input_pengadaan', [PengadaanController::class, 'index']);
-// Route::resource('/pengadaan1', PengadaanController::class);
-// Route::get('/pengadaan1/{id}', [PengadaanController::class, 'show'])->name('show');
+//detail pengadaan1<50juta
 Route::get('/pengadaan1/show1/{id}', [PengadaanController::class, 'show1'])->name('pengadaan1.show1');
 Route::get('/pengadaan1/show2/{id}', [PengadaanController::class, 'show2'])->name('pengadaan1.show2');
 Route::get('/pengadaan1/show3/{id}', [PengadaanController::class, 'show3'])->name('pengadaan1.show3');
@@ -142,8 +95,8 @@ Route::get('/pengadaan1/show28/{id}', [PengadaanController::class, 'show28'])->n
 //29
 //30
 Route::get('/pengadaan1/show31/{id}', [PengadaanController::class, 'show31'])->name('pengadaan1.show31');
-// Route::get('/lihat', PengadaanController::class, 'show1')->name('show1');
 
+//detail pengadaan1>50juta
 Route::get('/pengadaan2/show1/{id}', [PengadaanController::class, 'show1'])->name('pengadaan2.show1');
 Route::get('/pengadaan2/show2/{id}', [PengadaanController::class, 'show2'])->name('pengadaan2.show2');
 Route::get('/pengadaan2/show3/{id}', [PengadaanController::class, 'show3'])->name('pengadaan2.show3');
@@ -175,16 +128,12 @@ Route::get('/pengadaan2/show28/{id}', [PengadaanController::class, 'show28'])->n
 Route::get('/pengadaan2/show31/{id}', [PengadaanController::class, 'show31'])->name('pengadaan2.show31');
 // Route::get('/pengadaan1', [PengadaanController::class, 'index1'], [JadwalController::class, 'index1']);
 Route::get('/pengadaan1', [PengadaanController::class, 'index1']);
-// Route::get('/pengadaan1/detail/{id}', [PengadaanController::class, 'dt1']);
 Route::get('/pengadaan1cari', [PengadaanController::class, 'cari1'])->name('cari1');
-// Route::get('/pengadaan1cari', [JadwalController::class, 'cari1'])->name('cari1');
 
 Route::get('/pengadaan2', [PengadaanController::class, 'index2']);
 Route::get('/pengadaan2cari', [PengadaanController::class, 'cari2'])->name('cari2');
 
-// Route::get('/pengadaan1', [JadwalController::class, 'index1']);
-
-//print pengadaan 1
+//print pengadaan1 <50juta
 Route::get('print', [PdfController::class, 'index']);
 Route::get('pengadaan1/print1/{id}', [PdfController::class, 'nota_dinas1'])->name('pengadaan1.print1');
 Route::get('pengadaan1/print2/{id}', [PdfController::class, 'nota_dinas2'])->name('pengadaan1.print2');
@@ -220,7 +169,7 @@ Route::get('pengadaan1/print31/{id}', [PdfController::class, 'tor'])->name('peng
 Route::get('print_notadinas4', [PdfController::class, 'nota_dinas4']);
 Route::get('ba_pekerjaan', [PdfController::class, 'ba_pekerjaan']);
 
-//print pengadaan2
+//print pengadaan2 >50juta
 Route::get('pengadaan2/print1/{id}', [PdfController::class, 'nota_dinas1'])->name('pengadaan2.print1');
 Route::get('pengadaan2/print2/{id}', [PdfController::class, 'nota_dinas2'])->name('pengadaan2.print2');
 Route::get('pengadaan2/print3/{id}', [PdfController::class, 'undangan_persiapan'])->name('pengadaan2.print3');
@@ -248,23 +197,6 @@ Route::get('pengadaan2/print24/{id}', [PdfController::class, 'ba_peny_barang'])-
 //25
 Route::get('pengadaan2/print26/{id}', [PdfController::class, 'ba_pembayaran'])->name('pengadaan1.print26');
 
-
-
-// Route::get('/input_jadwal', function () {
-//     return view('admin.input_jadwal');
-// });
-
-// Route::get('/input_barang', function () {
-//     return view('admin.input_barang');
-// });
-// Route::resource('/pelaksanas', PelaksanaController::class);
-// Route::get('/input_pelaksana', function () {
-//     return view('admin.input_pelaksana');
-// });
-
-Route::get('/setting', function () {
-    return view('admin.setting');
-});
 //
 
 
@@ -275,131 +207,9 @@ Auth::routes(['verify' => false]);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/admin', [view::class, 'index']);
 Route::get('/masuk', [view::class, 'login']);
-Route::post('/save', [view::class, 'save']);
-// Route::delete('/admin/{id}',[view::class, 'delete']);
-// Route::delete('/admin/{id}', 'view@destroy')->name('admin.destroy');
-// Route::get('admin/hapus/{id}', 'view@hapus');
+Route::post('/save', [view::class, 'save']);;
 Route::get('admin/hapus/{id}', [view::class, 'hapus']);
-// Route::get('admin/edit/{id}', [PejabatController::class, '']);
-
-
-Route::get('/guest', [GuestController::class, 'index'])->name('guest');
 
 Route::get('/pengguna', [view::class, 'pengguna'])->name('pengguna');
-// Route::dele('/delete',[view::class, 'destroy']);
-// Route::delete('/pengguna/{id}', 'view@destroy')->name('pengguna.destroy');
 
-//tambahkan kode berikut
-// Route::resource('/asets', PemilikController::class);
-
-Route::resource('/delete', view::class);
-
-Route::resource('/pemilik', PemilikController::class);
-// Route::resource('/pengguna', View::class);
-// Route::resource('/save', App\Http\Controllers\view::class,'save');
-
-//print
-//1
-Route::get('/nota_dinas1', function () {
-    return view('print.nota_dinas1');
-});
-//2
-Route::get('/nota_dinas2', function () {
-    return view('print.nota_dinas2');
-});
-//3
-Route::get('/nota_dinas4', function () {
-    return view('print.nota_dinas4');
-});
-//4
-Route::get('/undangan_persiapan', function () {
-    return view('print.undangan_persiapan');
-});
-//5
-Route::get('/pakta_integritas', function () {
-    return view('print.pakta_integritas');
-});
-//6
-Route::get('/daftar_hadir', function () {
-    return view('print.daftar_hadir');
-});
-//7
-Route::get('/ba_evaluasi', function () {
-    return view('print.ba_evaluasi');
-});
-//8
-Route::get('/undangan_penawaran', function () {
-    return view('print.undangan_penawaran');
-});
-//9
-Route::get('/ba_pekerjaan', function () {
-    return view('print.ba_pekerjaan');
-});
-//10
-Route::get('/ba_penawaran', function () {
-    return view('print.ba_penawaran');
-});
-//11
-Route::get('/undangan_nego', function () {
-    return view('print.undangan_nego');
-});
-//12
-Route::get('/ba_nego', function () {
-    return view('print.ba_nego');
-});
-//13
-Route::get('/penetapan_penyedia', function () {
-    return view('print.penetapan_penyedia');
-});
-//14
-Route::get('/undangan_penyedia', function () {
-    return view('print.undangan_penyedia');
-});
-//15
-Route::get('/kuitansi_kontrak', function () {
-    return view('print.kuitansi_kontrak');
-});
-//16
-Route::get('/evaluasi_dok', function () {
-    return view('print.evaluasi_dok');
-});
-//17
-Route::get('/surat_perintah_kerja', function () {
-    return view('print.surat_perintah_kerja');
-});
-//18
-Route::get('/surat_perintah_mulai_kerja', function () {
-    return view('print.surat_perintah_mulai_kerja');
-});
-//19
-Route::get('/ba_serah_terima', function () {
-    return view('print.ba_serah_terima');
-});
-//20
-Route::get('/lampiran_ba_serah_terima', function () {
-    return view('print.lampiran_ba_serah_terima');
-});
-//21
-Route::get('/lap_proses_pengadaan', function () {
-    return view('print.lap_proses_pengadaan');
-});
-//22
-Route::get('/lampiran_lap_proses_peng', function () {
-    return view('print.lampiran_lap_proses_peng');
-});
-//23
-Route::get('/ba_peny_barang', function () {
-    return view('print.ba_peny_barang');
-});
-//24
-Route::get('/lampiran_ba_barang', function () {
-    return view('print.lampiran_ba_barang');
-});
-//25
-Route::get('/ba_pembayaran', function () {
-    return view('print.ba_pembayaran');
-});
-
-// Route::resource('/getusr',[App\Http\Controllers\AdminController::class, 'index']);
-
-// Route::get('/editpejabat/{}')
+// Route::resource('/delete', view::class);
